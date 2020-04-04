5efd161ca333747a5bbd8a0ce2d485b010b0b1a4
Tymofii Dolenko <sispo777@gmail.com>
2020-04-04 14:14:38 +0300

    Setup Styling + Fonts

---
+++ PersonalityTest/Styling/Fonts.swift
@@ -0,0 +1,29 @@
+//
+//  Fonts.swift
+//  PersonalityTest
+//
+//  Created by Tymofii Dolenko on 04.04.2020.
+//
+
+import UIKit
+
+public enum Font: String, CaseIterable {
+    case avenirNextBold = "AvenirNext-Bold"
+    case avenirNext = "AvenirNext-Regular"
+}
+
+public extension Font {
+    func ofSize(_ size: CGFloat) -> UIFont {
+        UIFont.init(name: self.rawValue, size: size)!
+    }
+}
+
+public extension UIFont {
+    class func avenirOfSize(_ size: CGFloat) -> UIFont {
+        Font.avenirNext.ofSize(size)
+    }
+    class func avenirBoldOfSize(_ size: CGFloat) -> UIFont {
+        Font.avenirNextBold.ofSize(size)
+    }
+}
+
