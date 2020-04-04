596bfaba6320f511069e1413f9bef8cce6b9059e
Tymofii Dolenko <sispo777@gmail.com>
2020-04-04 14:37:04 +0300

    QuestionaryViewController + LoadView/Layout

--- PersonalityTest/Questionary/QuestionaryViewController+Layout.swift
+++ PersonalityTest/Questionary/QuestionaryViewController+Layout.swift
@@ -8,12 +8,76 @@
 import UIKit
 import SnapKit
 
+// MARK:  Load View
 extension QuestionaryViewController {
     
     override func loadView() {
         let view = UIView()
+        view.backgroundColor = .screenDarkBackground
         
+        let topBar = UIView()
+        topBar.backgroundColor = .clear
         
+        view.addSubview(topBar)
+        topBar.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        let title = makeTitle()
+        topBar.addSubview(title)
+        
+        title.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        let nextButton = makeNextButton()
+        view.addSubview(nextButton)
+        nextButton.snp.makeConstraints { (make) in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+        }
+        
+        let tableView = UITableView()
+        tableView.backgroundColor = .screenDarkBackground
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.top.equalTo(topBar.snp.bottom)
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.bottom.equalTo(nextButton.snp.top)
+        }
+        
+        self.view = view
+        self.topBar = topBar
+        self.titleLbl = title
+        self.tableView = tableView
+        self.nextBtn = nextBtn
     }
     
+}
+
+// MARK:  View Factory Methods
+extension QuestionaryViewController {
+    
+    private func makeTitle() -> UILabel {
+        let label = UILabel()
+        label.font = .avenirBoldOfSize(20)
+        label.text = "Welcome"
+        return label
+    }
+    
+    private func makeNextButton() -> UIButton {
+        let button = UIButton()
+        button.backgroundColor = .accentColor
+        button.titleLabel?.font = .avenirBoldOfSize(20)
+        button.titleLabel?.textColor = .white
+        button.setTitle("Next", for: .normal)
+        return button
+    }
 }
--- PersonalityTest/Questionary/QuestionaryViewController.swift
+++ PersonalityTest/Questionary/QuestionaryViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class QuestionaryViewController: UIViewController {
     
+    weak var titleLbl: UILabel!
+    weak var topBar: UIView!
+    weak var nextBtn: UIButton!
+    weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
