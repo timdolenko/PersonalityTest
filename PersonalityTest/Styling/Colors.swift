7a649bd2828b448ba1549e583376124edc31b3bf
Tymofii Dolenko <sispo777@gmail.com>
2020-04-04 13:52:02 +0300

    Setup Styling + Colors

--- PersonalityTest/Styling/Colors.swift
+++ PersonalityTest/Styling/Colors.swift
@@ -8,5 +8,19 @@
 import UIKit
 
 public extension UIColor {
+    class var screenDarkBackground: UIColor {
+        .black
+    }
     
+    class var elevatedDarkBackground: UIColor {
+        UIColor(red: 18.0/255.0, green: 18.0/255.0, blue: 18.0/255.0, alpha: 1.0)
+    }
+    
+    class var textLightColor: UIColor {
+        .white
+    }
+    
+    class var accentColor: UIColor {
+        UIColor(red: 195.0/255.0, green: 143.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    }
 }
