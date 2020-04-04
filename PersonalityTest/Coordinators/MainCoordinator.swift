50018aa0f7b2bb7220360f33c1c5d1a79568e001
Tymofii Dolenko <sispo777@gmail.com>
2020-04-04 13:36:31 +0300

    Main Coordinator

--- PersonalityTest/Coordinators/MainCoordinator.swift
+++ PersonalityTest/Coordinators/MainCoordinator.swift
@@ -5,4 +5,17 @@
 //  Created by Tymofii Dolenko on 04.04.2020.
 //
 
-import Foundation
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+    }
+}
