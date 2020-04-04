d38c6bc3420c01f690d36fb8812e8beeb0f960c0
Tymofii Dolenko <sispo777@gmail.com>
2020-04-04 12:52:35 +0300

    Initial Commit

---
+++ PersonalityTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityTest
+//
+//  Created by Tymofii Dolenko on 04.04.2020.
+//  Copyright © 2020 Tymofii Dolenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
