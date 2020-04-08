d7254f64d7c7c454f0c63e064718ebb4d9a0ed9f
Tymofii Dolenko <sispo777@gmail.com>
2020-04-08 18:10:49 +0300

    NetworkServiceTests

--- Networking/NetworkingTests/NetworkServiceTests.swift
+++ Networking/NetworkingTests/NetworkServiceTests.swift
@@ -26,17 +26,39 @@ class NetworkServiceTests: XCTestCase {
         case someError
     }
     
-    func test_whenMockDataPassed_shouldReturnProperResponse() {
-        let config = NetworkConfigurableMock()
-        let expectation = self.expectation(description: "Should return correct data")
-        
+    private var mockConfig = NetworkConfigurableMock()
+    private var mockGetValidEndpoint = EndpointMock(path: "/mockPath", method: .get)
+    
+    private func makeSessionManagerMockWithValidData() -> (data: Data, manager: NetworkSessionManagerMock) {
         let expectedResponseData = "Data".data(using: .utf8)!
         
-        let mock = NetworkSessionManagerMock(response: nil, data: expectedResponseData, error: nil)
+        let mock = NetworkSessionManagerMock(
+            response: nil,
+            data: expectedResponseData,
+            error: nil
+        )
         
-        let sut = NetworkService(config: config, sessionManager: mock)
+        return (data: expectedResponseData, manager: mock)
+    }
+    
+    private func makeSessionManagerMock(with error: NSError) -> NetworkSessionManagerMock {
+        NetworkSessionManagerMock(
+            response: nil,
+            data: nil,
+            error: error
+        )
+    }
+    
+    func test_whenMockDataPassed_shouldReturnProperResponse() {
+        //given
+        let expectation = self.expectation(description: "Should return correct data")
         
-        _ = sut.request(endpoint: EndpointMock(path: "/mockPath", method: .get), completion: { (result) in
+        let (expectedResponseData, sessionManager) = makeSessionManagerMockWithValidData()
+        
+        let sut = NetworkService(config: mockConfig, sessionManager: sessionManager)
+        
+        //when
+        _ = sut.request(endpoint: mockGetValidEndpoint, completion: { (result) in
             
             switch result {
             case let .success(data):
@@ -46,28 +68,92 @@ class NetworkServiceTests: XCTestCase {
                 XCTFail("Should return proper response")
             }
         })
+        //then
+        wait(for: [expectation], timeout: 0.1)
+    }
+    
+    func test_onCancelledError_shouldReturnCancelledError() {
+        //given
+        let expectation = self.expectation(description: "Should return cancelled error")
+        
+        let cancelledError = NSError(domain: "network", code: NSURLErrorCancelled, userInfo: nil)
+        
+        let sessionManager = makeSessionManagerMock(with: cancelledError)
+        
+        let sut = NetworkService(config: mockConfig, sessionManager: sessionManager)
+        
+        //when
+        _ = sut.request(endpoint: mockGetValidEndpoint, completion: { (result) in
+            
+            switch result {
+            case .success:
+                XCTFail("Should not happen")
+            case let .failure(error):
+                guard case NetworkError.cancelled = error else {
+                    XCTFail("Should return NetworkError.cancelled error")
+                    return
+                }
+                
+                expectation.fulfill()
+            }
+        })
         
         wait(for: [expectation], timeout: 0.1)
     }
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func test_whenMalformedUrlPassed_shouldReturnUrlGenerationError() {
+        //given
+        let expectation = self.expectation(description: "Should return urlGeneration error")
+        
+        let (_, sessionManager) = makeSessionManagerMockWithValidData()
+        
+        let sut = NetworkService(config: mockConfig, sessionManager: sessionManager)
+        
+        //when
+        _ = sut.request(endpoint: EndpointMock(path: "_+)+()+__?", method: .get), completion: { (result) in
+            
+            switch result {
+            case .success:
+                XCTFail("Should not happen")
+            case let .failure(error):
+                guard case NetworkError.urlGeneration = error else {
+                    XCTFail("Should return url generation error")
+                    return
+                }
+                
+                expectation.fulfill()
+            }
+        })
+        //then
+        wait(for: [expectation], timeout: 0.1)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_onNotConnectedToInternetError_shouldReturnNotConnectedError() {
+        //given
+        let expectation = self.expectation(description: "Should return notConnected error")
+        
+        let error = NSError(domain: "network", code: NSURLErrorNotConnectedToInternet, userInfo: nil)
+        
+        let sessionManager = makeSessionManagerMock(with: error)
+        
+        let sut = NetworkService(config: mockConfig, sessionManager: sessionManager)
+        
+        //when
+        _ = sut.request(endpoint: mockGetValidEndpoint, completion: { (result) in
+            switch result {
+            case .success:
+                XCTFail("Should not happen")
+            case let .failure(error):
+                guard case NetworkError.notConnected = error else {
+                    XCTFail("Should return notConnected error")
+                    return
+                }
+                
+                expectation.fulfill()
+            }
+        })
+        
+        //then
+        wait(for: [expectation], timeout: 0.1)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
